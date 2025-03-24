6b64b971b4ac366dbea7402351e754f37c3dc029
Metared04 <e.metared@gmail.com>
2025-03-24 14:19:17 +0100

    exo 2

--- ConsoleApp1/ConsoleApp1/Programm.cs
+++ ConsoleApp1/ConsoleApp1/Programm.cs
@@ -8,17 +8,45 @@ class Programm
 {
     static void Main(string[] args)
     {
+        Console.WriteLine("Votre salaire => ");
+        string salaireUtilisateur = Console.ReadLine();
+        bool testSalaire = int.TryParse(salaireUtilisateur, out int nombre);
+        int leSalaireUtilisateur = 0;
+        if (testSalaire == true)
+        {
+            leSalaireUtilisateur = int.Parse(salaireUtilisateur);
+        }
 
-        string salary = args[0];
-        string taux = args[1];  
+        Console.WriteLine("Le tax => ");
+        string taxUtilisateur = Console.ReadLine();
+        bool testTax = int.TryParse(taxUtilisateur, out int autreNombre);
+        int laTaxUtilisateur = 0;
+        if (testTax == true)
+        {
+            laTaxUtilisateur = int.Parse(taxUtilisateur);
+        }
 
-        double mensuelSalary = Convert.ToDouble(salary) / 12;
-        double tax = double.Parse(taux);
-        double netSalary = mensuelSalary * (1-tax);
+        if(testSalaire == false || testTax == false)
+        {
+            Console.WriteLine("Tant pis pour toi ^^");
+        }
+        else
+        {
+            Console.WriteLine("BG en vdv ^^");
+        }
 
-        Console.WriteLine("Salaire mensuel :" + mensuelSalary);
-        Console.WriteLine("Salaire net : " + netSalary);
-        Console.ReadLine();
+        if(leSalaireUtilisateur >= 50000)
+        {
+            Console.WriteLine("Fais un don sur ma chaine twitch stp.............................................................");
+        }
+        else if (leSalaireUtilisateur / 12 <= 1500)
+        {
+            Console.WriteLine("T'as pas d'argent mskn");
+        }
+        else if(leSalaireUtilisateur >=30000 && leSalaireUtilisateur <= 40000)
+        {
+            Console.WriteLine("Go au CESI hein ^^^^^^^^^^^^^^^^^^^^^^");
+        }
     }
 
 }

df32ce03b0b7a2c17e312190332b2e7c93aad73d
Metared04 <e.metared@gmail.com>
2025-03-24 11:38:12 +0100

    Etape 1 et 2 ?

---
+++ ConsoleApp1/ConsoleApp1/Programm.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Programm
+{
+    static void Main(string[] args)
+    {
+
+        string salary = args[0];
+        string taux = args[1];  
+
+        double mensuelSalary = Convert.ToDouble(salary) / 12;
+        double tax = double.Parse(taux);
+        double netSalary = mensuelSalary * (1-tax);
+
+        Console.WriteLine("Salaire mensuel :" + mensuelSalary);
+        Console.WriteLine("Salaire net : " + netSalary);
+        Console.ReadLine();
+    }
+
+}

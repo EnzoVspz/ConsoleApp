b3c0c63377fc55a3a9957d29f536ae8706bd6bbb
Metared04 <e.metared@gmail.com>
2025-03-25 10:31:16 +0100

    Dernier commit

--- ConsoleApp1/ConsoleApp1/Programm.cs
+++ ConsoleApp1/ConsoleApp1/Programm.cs
@@ -1,54 +1,60 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
+using ConsoleApp1;
 
 class Programm
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("oui");
-        ISet<String> mois = new HashSet<String>();
+        User test = new User(1, "Jess", "R", 23, 0 , 30);
 
-        mois.Add("Janvier");
-        mois.Add("Fevrier");
-        mois.Add("Mars");
-        mois.Add("Avril");
-        mois.Add("Mai");
-        mois.Add("Juin");
-        mois.Add("Juillet");
-        mois.Add("Aout");
-        mois.Add("Septembre");
-        mois.Add("Octobre");
-        mois.Add("Novembre");
-        mois.Add("Decembre");
-
-        float leSalaireUtilisateur = 75000;
-        float laTax = 37;
-        float taux = 1 - (laTax / 100);
-        float leSalaire = (leSalaireUtilisateur / 12) * taux;
-        float bonus = leSalaire + (leSalaireUtilisateur * 10 / 100);
-        float rien = 0;
-
-        Console.WriteLine("oui");
-        foreach(String s in mois)
+        Console.WriteLine("Bonjour " + test.firstName + "\nFaire comme avant [1]\nCalculer les interets composes [2]\n");
+        
+        try
         {
-            if(s == "Decembre")
+            int choixUser = int.Parse(Console.ReadLine());
+            float salaire = 0;
+            float taux = 0;
+            if (choixUser < 1 || choixUser > 2) 
             {
-                Console.WriteLine(s + " => " + bonus);
+                Console.WriteLine("Le chiffre doit etre entre 1 et 2 !");
             }
-            else if(s == "Aout")
+            else if (choixUser == 1)
             {
-                Console.WriteLine(s + " => " + rien);
+                Console.WriteLine("On va faire comme avant ...\n");
+                Console.WriteLine("Votre salaire ? => ");
+                salaire = float.Parse(Console.ReadLine());
+                taux = (100 - float.Parse(Console.ReadLine()) / 100);
             }
             else
             {
-                Console.WriteLine(s + " => " + leSalaire);
+                Console.WriteLine("Calculons les interets composes.\n");
+                Console.WriteLine("Votre salaire ? => ");
+                salaire = float.Parse(Console.ReadLine());
+                Console.WriteLine("Votre taux ? => ");
+                taux = (100 - float.Parse(Console.ReadLine()) / 100);
+                Console.WriteLine("Temps ? => ");
+                int annee = int.Parse(Console.ReadLine());
+                for (int i = 0; i < annee; i++)
+                {
+                    Console.WriteLine("Annee " + i+1 + " : salaire => " + salaire);
+                    salaire = salaire + salaire * taux;
+                }
             }
         }
+        catch (FormatException e)
+        {
+            Console.WriteLine("C'est pas un chiffre. Fin du programme...\n" + e.Message);
+            Environment.Exit(-1);
+        }
+        
+        Console.WriteLine("Fin du programme...");
+        Console.ReadLine();
     }
-
     public static void Un()
     {
         Console.WriteLine("Votre salaire => ");
@@ -89,6 +95,63 @@ class Programm
         else if (leSalaireUtilisateur >= 30000 && leSalaireUtilisateur <= 40000)
         {
             Console.WriteLine("Go au CESI hein ^^^^^^^^^^^^^^^^^^^^^^");
+        }
+    }
+
+    public static void Deux()
+    {
+        Console.WriteLine("oui");
+        ISet<String> mois = new HashSet<String>();
+
+        mois.Add("Janvier");
+        mois.Add("Fevrier");
+        mois.Add("Mars");
+        mois.Add("Avril");
+        mois.Add("Mai");
+        mois.Add("Juin");
+        mois.Add("Juillet");
+        mois.Add("Aout");
+        mois.Add("Septembre");
+        mois.Add("Octobre");
+        mois.Add("Novembre");
+        mois.Add("Decembre");
+
+        float leSalaireUtilisateur = 75000;
+        float laTax = 37;
+        float taux = 1 - (laTax / 100);
+        float leSalaire = (leSalaireUtilisateur / 12) * taux;
+        float bonus = leSalaire + (leSalaireUtilisateur * 10 / 100);
+        float rien = 0;
+
+        Console.WriteLine("oui");
+        foreach (String s in mois)
+        {
+            if (s == "Decembre")
+            {
+                Console.WriteLine(s + " => " + bonus);
+            }
+            else if (s == "Aout")
+            {
+                Console.WriteLine(s + " => " + rien);
+            }
+            else
+            {
+                Console.WriteLine(s + " => " + leSalaire);
+            }
+        }
+    }
+
+    public static void Trois()
+    {
+        try
+        {
+            Console.WriteLine("Pourcentage => ");
+            int pourcentage = int.Parse(Console.ReadLine());
+            Console.WriteLine("");
+        }
+        catch (DivideByZeroException e)
+        {
+            Environment.Exit(-1);
         }
     }
 
---
+++ ConsoleApp1/ConsoleApp1/User.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class User
+    {
+        public int id;
+        public string firstName;
+        public string lastName;
+        public int old;
+        public float salary;
+        public float tax;
+
+        public User(int id, string firstName, string lastName, int old, float salary, float tax)
+        {
+            this.id = id;
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.old = old;
+            this.salary = salary;
+            this.tax = tax;
+        }
+
+        public void oui()
+        {
+
+        }
+
+    }
+}
